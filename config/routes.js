ef98d0223d65067794da8d8565fcc87794b41ffe
Bianca Jemsten <biancajemsten@gmail.com>
2018-06-03 14:52:17 +0100

    set up login

--- config/routes.js
+++ config/routes.js
@@ -3,6 +3,7 @@ const router   = express.Router();
 
 const static = require('../controllers/static');
 const registrations = require('../controllers/registrations');
+const sessions = require('../controllers/sessions');
 
 // handle a request
 router.get('/', (req, res) => res.render('home', {
@@ -13,6 +14,8 @@ router.route('/register')
   .get(registrations.new)
   .post(registrations.create);
 
-
+router.route('/login')
+  .get(sessions.new)
+  .post(sessions.create);
 
 module.exports = router;
