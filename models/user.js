9f435f63e5118e4c3b584a3201227a4713ad702d
Bianca Jemsten <biancajemsten@gmail.com>
2018-06-03 22:21:28 +0100

    Edited user profile

--- models/user.js
+++ models/user.js
@@ -5,6 +5,7 @@ const userSchema = new mongoose.Schema({
   username: String,
   email: { type: String, unique: true, required: true},
   age: String,
+  nationality: String,
   currentlyIn: String,
   password: { type: String, required: true }
 },{
